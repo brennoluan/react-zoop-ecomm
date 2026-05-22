d38cb480750936b73cec2a75511225d71b53f395
Brenno Luan <brennoluan8530@gmail.com>
2026-05-22 13:28:40 -0300

    Memoized `productForCart` with `useMemo` and added custom `memo` comparison function in `ProductCardWithFavorites` to optimize rendering.

--- src/components/ProductCardWithFavorites.tsx
+++ src/components/ProductCardWithFavorites.tsx
@@ -1,4 +1,4 @@
-import React, { memo } from 'react';
+import React, { memo, useMemo } from 'react';
 import ProductCard from './ProductCard';
 import FavoritesButton from './FavoritesButton';
 
@@ -32,12 +32,15 @@ const ProductCardWithFavorites: React.FC<ProductCardWithFavoritesProps> = memo(
 		showAddToCart = false,
 	}) => {
 		// Convert product to the format expected by ProductCard
-		const productForCart = {
-			id: Number(product.id),
-			name: product.name,
-			price: product.price,
-			image: product.image,
-		};
+		const productForCart = useMemo(
+			() => ({
+				id: Number(product.id),
+				name: product.name,
+				price: product.price,
+				image: product.image,
+			}),
+			[product]
+		);
 
 		return (
 			<div className='relative'>
@@ -57,6 +60,9 @@ const ProductCardWithFavorites: React.FC<ProductCardWithFavoritesProps> = memo(
 				/>
 			</div>
 		);
+	},
+	(prevProps, nextProps) => {
+		return prevProps.product.id === nextProps.product.id;
 	}
 );
 
